ca9699f846bebd8afe98e0754cf9412b4d8f6d69
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-06-27 22:43:43 -0400

    commit tree ae11a0333598c596572c1c018872314e75389701

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
+  const isInCart = inCart ? "in-cart" : ""
+
   return (
-    <li className="">
+    <li className={isInCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => setInCart(!inCart)}>Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setCategory] = useState("All")
+  function filterCategory(e) {
+    setCategory(e.target.value)
+  }
+
+  function filterItem(items) {
+    if (selectedCategory === "All") {
+      return items.map((item) => (
+        <Item key={item.id} name={item.name} category={item.category} />
+      ))
+    } else {
+      return items.filter(item => item.category === selectedCategory).map(item => <Item key={item.id} name={item.name} category={item.category}/>)
+    }
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={filterCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +28,10 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {/* {items.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        ))} */}
+        {filterItem(items)}
       </ul>
     </div>
   );
